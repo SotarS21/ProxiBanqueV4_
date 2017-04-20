33e146b63be4fdf364a679d8789ef1904572049f
andy0Y <acrepinpro@gmail.com>
2017-04-20 16:16:34 +0200

    added dao and service client

--- ProxiBanque/src/main/java/org/ProxiBanque/dao/ICRUDClient.java
+++ ProxiBanque/src/main/java/org/ProxiBanque/dao/ICRUDClient.java
@@ -1,8 +1,11 @@
 package org.ProxiBanque.dao;
 
+import java.util.List;
+
 import org.ProxiBanque.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICRUDClient extends JpaRepository<Client, Long> {
 
+	public List<Client> findByConseiller_Id(Long idConseiller);
 }
---
+++ ProxiBanque/src/main/java/org/ProxiBanque/service/IServiceClient.java
@@ -0,0 +1,8 @@
+package org.ProxiBanque.service;
+
+import org.ProxiBanque.model.Client;
+
+public interface IServiceClient {
+
+	public Client findOne(Long id);
+}
