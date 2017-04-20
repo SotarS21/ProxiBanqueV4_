d7ad0e04affebf4373cad77505625128098aaba8
arthemise jonas <arthemise.jonas@gmail.com>
2017-04-20 18:06:52 +0200

    [ADDOTATION FOOTPRINT]

--- ProxiBanque/src/main/java/org/ProxiBanque/model/Footprint.java
+++ ProxiBanque/src/main/java/org/ProxiBanque/model/Footprint.java
@@ -1,9 +1,13 @@
 package org.ProxiBanque.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="FOOTPRINT")
 public class Footprint <T>{
 	
 	@Id
