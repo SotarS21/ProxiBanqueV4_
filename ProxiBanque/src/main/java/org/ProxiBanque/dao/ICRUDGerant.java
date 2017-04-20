6b552e0e51fcf9428c12244f31bd1de13cc4d27b
andy0Y <acrepinpro@gmail.com>
2017-04-20 15:58:30 +0200

    delete dao

--- ProxiBanque/src/main/java/org/ProxiBanque/dao/ICRUDGerant.java
+++
@@ -1,8 +0,0 @@
-package org.ProxiBanque.dao;
-
-import org.ProxiBanque.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICRUDGerant extends JpaRepository<Gerant, Long> {
-
-}
