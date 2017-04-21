eb0f496f97cda2d1b5af030fc1fd52468f2cff05
arthemise jonas <arthemise.jonas@gmail.com>
2017-04-21 11:48:53 +0200

    [REAL ADD TEST]

--- ProxiBanque/src/test/java/org/ProxiBanque/service/AdvisorTestService.java
+++ ProxiBanque/src/test/java/org/ProxiBanque/service/AdvisorTestService.java
@@ -1,5 +1,6 @@
 package org.ProxiBanque.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -11,6 +12,8 @@ import org.ProxiBanque.model.Advisor;
 import org.ProxiBanque.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,27 +24,74 @@ public class AdvisorTestService {
 
 	@Autowired
 	IServiceAdvisor service;
+	
 
-//	@Test
-//	public void testAdvisorService() {
-//		assertNotNull(service);
-//	}
-//
-//	@Test
-//	public void testAddAdvisor() {
-//		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
-//
-//		service.save(advisor);
-//		List<Advisor> advisors = service.findByLastNameAndFirstNameAllIgnoreCase("jean", null);
-//
-//		assertTrue(advisors.size() >= 1);
-//	}
-//
-//	@Test
-//	public void testListFindByName() {
-//		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
-//
-//		service.save(advisor);
-//		List<Advisor> advisors = service.findByLastNameAndFirstNameAllIgnoreCase("jean", null);
-//	}
+	private static final Logger logger = LoggerFactory.getLogger(AdvisorTestService.class);
+
+
+	@Test
+	public void testAdvisorService() {
+		assertNotNull(service);
+	}
+
+	@Test
+	public void testAddAdvisor() {
+		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
+
+		advisor.setCellphone("09784556");
+		advisor.setEmail("jean.valjean@gmail.com");
+		
+		int presize = service.findAll().size();
+		service.save(advisor);
+		int postSize = service.findAll().size();
+		assertTrue(presize < postSize);
+	}
+
+	@Test
+	public void testListFindByName() {
+		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
+
+		advisor.setCellphone("09784556");
+		advisor.setEmail("jean.valjean@gmail.com");
+
+		service.save(advisor);
+		int size = service.findByLastNameAndFirstNameAllIgnoreCase( "Valjean", "jean").size();
+		assertTrue(size > 0);
+	}
+	
+	
+	@Test
+	public void testUpdate()
+	{
+		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
+		advisor.setCellphone("09784556");
+		advisor.setEmail("jean.valjean@gmail.com");
+
+		service.save(advisor);
+		List<Advisor> advisors = service.findByLastNameAndFirstNameAllIgnoreCase( "Valjean", "jean");
+		
+		Advisor advisor2 = advisors.get(advisors.size()-1);
+		
+		advisor2.setAddress(new Address("78", "77420", "Champ sur Marne"));
+		service.update(advisor2);
+		List<Advisor> advisors2 = service.findByLastNameAndFirstNameAllIgnoreCase( "Valjean", "jean");
+//		System.out.println("------------------------------------------- advisor = "+advisor);
+//		System.out.println("------------------------------------------- advisor2 = "+advisors2.get(advisors.size()-1));
+		assertFalse(advisor.getAddress().getZipCode().equals(advisors2.get(advisors.size()-1).getAddress().getZipCode()));
+	}
+	
+	@Test
+	public void testDelete()
+	{
+		Advisor advisor = new Advisor("Jean", "Valjean", new Address("10", "75010", "Paris"), new User("jean", "jean"));
+		advisor.setCellphone("09784556");
+		advisor.setEmail("jean.valjean@gmail.com");
+
+		service.save(advisor);
+		List<Advisor> advisors = service.findByLastNameAndFirstNameAllIgnoreCase( "Valjean", "jean");
+		int sizeBefor = advisors.size();
+		service.delete(advisors.get(0).getId());
+		int sizeAfter = service.findByLastNameAndFirstNameAllIgnoreCase( "Valjean", "jean").size();
+		assertTrue(sizeBefor > sizeAfter);
+	}
 }
