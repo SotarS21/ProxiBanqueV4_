e81adc7f7d216557c67d8026de15daa786a74e79
andy0Y <acrepinpro@gmail.com>
2017-04-23 10:24:55 +0200

    finish WS advisor, no test yet

--- ProxiBanque/src/main/java/org/ProxiBanque/wsrest/service/ServiceAdvisorWSRest.java
+++ ProxiBanque/src/main/java/org/ProxiBanque/wsrest/service/ServiceAdvisorWSRest.java
@@ -8,11 +8,8 @@ import javax.ws.rs.core.Response;
 
 import org.ProxiBanque.service.IServiceAdvisor;
 import org.ProxiBanque.service.ServiceAdvisor;
-import org.ProxiBanque.wsrest.bean.Address;
+import org.ProxiBanque.wsrest.adapter.WSRestAdapter;
 import org.ProxiBanque.wsrest.bean.Advisor;
-import org.ProxiBanque.wsrest.bean.Client;
-import org.ProxiBanque.wsrest.bean.CurrentAccount;
-import org.ProxiBanque.wsrest.bean.SavingAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,36 +34,8 @@ public class ServiceAdvisorWSRest implements IServiceAdvisorWSRest {
 	public Advisor findOne(String id) {
 
 		logger.debug("Call advisor findOne: id " + id);
-		org.ProxiBanque.model.Advisor advisorTemp = serviceAdvisor.findOne(Long.valueOf(id));
-		Address address = new Address();
-		address.setNumber(advisorTemp.getAddress().getNumber());
-		address.setTown(advisorTemp.getAddress().getTown());
-		address.setZipCode(advisorTemp.getAddress().getZipCode());
-		Advisor advisor = new Advisor(advisorTemp.getFirstName(), advisorTemp.getLastName(), address);
-
-		for (org.ProxiBanque.model.Client client : advisorTemp.getClients()) {
-
-			address.setNumber(client.getAddress().getNumber());
-			address.setTown(client.getAddress().getTown());
-			address.setZipCode(client.getAddress().getZipCode());
-
-			CurrentAccount current = new CurrentAccount(client.getCurrentAccount().getDecouvert(),
-					client.getCurrentAccount().getSold());
-			current.setType(client.getCurrentAccount().getType());
-
-			SavingAccount saving = new SavingAccount(client.getSafeAccount().getSold(),
-					client.getSafeAccount().getTauxDeRemuneration(), client.getSafeAccount().getDecouvert());
-			saving.setType(client.getSafeAccount().getType());
-
-			Client client2 = new Client(client.getFirstName(), client.getLastName(), address, client.getType(),
-					client.isRitch());
-			client2.setCurrentAccount(current);
-			client2.setSafeAccount(saving);
-			advisor.addClient(client2);
-
-		}
-
-		return advisor;
+		
+		return WSRestAdapter.marshallAdvisor(serviceAdvisor.findOne(Long.valueOf(id)));
 	}
 
 	@Override
@@ -74,42 +43,15 @@ public class ServiceAdvisorWSRest implements IServiceAdvisorWSRest {
 
 		logger.debug("Call advisor findAll()");
 
+		List<Advisor> marshallList = new ArrayList<Advisor>();
 		List<org.ProxiBanque.model.Advisor> list = serviceAdvisor.findAll();
-		List<Advisor> listAdvisor = new ArrayList<Advisor>();
-		
-		for (org.ProxiBanque.model.Advisor advisorTemp : list) {
+		for (org.ProxiBanque.model.Advisor advisor : list) {
 			
-			Address address = new Address();
-			address.setNumber(advisorTemp.getAddress().getNumber());
-			address.setTown(advisorTemp.getAddress().getTown());
-			address.setZipCode(advisorTemp.getAddress().getZipCode());
-			Advisor advisor = new Advisor(advisorTemp.getFirstName(), advisorTemp.getLastName(), address);
-
-			for (org.ProxiBanque.model.Client client : advisorTemp.getClients()) {
-
-				address.setNumber(client.getAddress().getNumber());
-				address.setTown(client.getAddress().getTown());
-				address.setZipCode(client.getAddress().getZipCode());
-
-				CurrentAccount current = new CurrentAccount(client.getCurrentAccount().getDecouvert(),
-						client.getCurrentAccount().getSold());
-				current.setType(client.getCurrentAccount().getType());
-
-				SavingAccount saving = new SavingAccount(client.getSafeAccount().getSold(),
-						client.getSafeAccount().getTauxDeRemuneration(), client.getSafeAccount().getDecouvert());
-				saving.setType(client.getSafeAccount().getType());
-
-				Client client2 = new Client(client.getFirstName(), client.getLastName(), address, client.getType(),
-						client.isRitch());
-				client2.setCurrentAccount(current);
-				client2.setSafeAccount(saving);
-				advisor.addClient(client2);
-
-				listAdvisor.add(advisor);
-			}
+			marshallList.add(WSRestAdapter.marshallAdvisor(advisor));
+			
 		}
-
-		return listAdvisor;
+		
+		return marshallList;
 	}
 
 	@Override
@@ -117,42 +59,15 @@ public class ServiceAdvisorWSRest implements IServiceAdvisorWSRest {
 
 		logger.debug("Call advisor  findByLastNameAndFirstNameAllIgnoreCase() last name : " + lastName
 				+ "| first name :" + firstName);
+		List<Advisor> marshallList = new ArrayList<Advisor>();
 		List<org.ProxiBanque.model.Advisor> list = serviceAdvisor.findByLastNameAndFirstNameAllIgnoreCase(firstName, firstName);
-		List<Advisor> listAdvisor = new ArrayList<Advisor>();
-		
-		for (org.ProxiBanque.model.Advisor advisorTemp : list) {
+		for (org.ProxiBanque.model.Advisor advisor : list) {
 			
-			Address address = new Address();
-			address.setNumber(advisorTemp.getAddress().getNumber());
-			address.setTown(advisorTemp.getAddress().getTown());
-			address.setZipCode(advisorTemp.getAddress().getZipCode());
-			Advisor advisor = new Advisor(advisorTemp.getFirstName(), advisorTemp.getLastName(), address);
-
-			for (org.ProxiBanque.model.Client client : advisorTemp.getClients()) {
-
-				address.setNumber(client.getAddress().getNumber());
-				address.setTown(client.getAddress().getTown());
-				address.setZipCode(client.getAddress().getZipCode());
-
-				CurrentAccount current = new CurrentAccount(client.getCurrentAccount().getDecouvert(),
-						client.getCurrentAccount().getSold());
-				current.setType(client.getCurrentAccount().getType());
-
-				SavingAccount saving = new SavingAccount(client.getSafeAccount().getSold(),
-						client.getSafeAccount().getTauxDeRemuneration(), client.getSafeAccount().getDecouvert());
-				saving.setType(client.getSafeAccount().getType());
-
-				Client client2 = new Client(client.getFirstName(), client.getLastName(), address, client.getType(),
-						client.isRitch());
-				client2.setCurrentAccount(current);
-				client2.setSafeAccount(saving);
-				advisor.addClient(client2);
-
-				listAdvisor.add(advisor);
-			}
+			marshallList.add(WSRestAdapter.marshallAdvisor(advisor));
+			
 		}
-
-		return listAdvisor;
+		
+		return marshallList;
 	}
 
 	@Override
@@ -161,7 +76,8 @@ public class ServiceAdvisorWSRest implements IServiceAdvisorWSRest {
 		if (advisor != null) {
 
 			logger.debug("Call advisor save () : client = " + advisor);
-			serviceAdvisor.save(advisor);
+			org.ProxiBanque.model.Advisor unmarhallAdvisor = WSRestAdapter.unmarshallAdvisor(advisor);
+			serviceAdvisor.save(unmarhallAdvisor);
 			return Response.ok().build();
 		}
 		return Response.notModified("null object").build();
@@ -173,7 +89,8 @@ public class ServiceAdvisorWSRest implements IServiceAdvisorWSRest {
 		if (advisor != null) {
 
 			logger.debug("Call advisor save () : client = " + advisor);
-			serviceAdvisor.save(advisor);
+			org.ProxiBanque.model.Advisor unmarhallAdvisor = WSRestAdapter.unmarshallAdvisor(advisor);
+			serviceAdvisor.save(unmarhallAdvisor);
 			return Response.ok().build();
 		}
 		return Response.notModified("null object").build();
