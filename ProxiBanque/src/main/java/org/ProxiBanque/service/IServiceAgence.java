17f563b53c7bdd6cdf98362e3eac15914c4bb1e9
arthemise jonas <arthemise.jonas@gmail.com>
2017-04-24 15:43:55 +0200

    [ADD JAVADOC] ajout e la javadoc dans le service Agence

--- ProxiBanque/src/main/java/org/ProxiBanque/service/IServiceAgence.java
+++ ProxiBanque/src/main/java/org/ProxiBanque/service/IServiceAgence.java
@@ -2,6 +2,14 @@ package org.ProxiBanque.service;
 
 import org.ProxiBanque.model.Agence;
 
+/**
+ * Interface de service concernant l'agence, elle est appeler dans les controllers addapter ou dans un webService .
+ * 
+ * 
+* @author kevin, jonas, andy, mathieu
+* @version 1.0
+*/
+
 public interface IServiceAgence {
 
 	public Agence findOne(Long id);
